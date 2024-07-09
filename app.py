ec8e665ddb880ee5dc896d91e0d7c4737f931252
mahakiim <muhammadhakim.ed@gmail.com>
2024-07-09 10:04:42 +0700

    Testing register web dummy

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# In-memory database for storing registered users (for simplicity)
+users = []
+
+@app.route('/')
+def home():
+    return "Welcome to the Home Page!"
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        # Simple user storage
+        users.append({'username': username, 'password': password})
+        return redirect(url_for('home'))
+    
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
